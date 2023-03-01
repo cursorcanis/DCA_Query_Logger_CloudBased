5465e8eee942670bbcd8dc41d818881b7243ebe5
Alfredo Alea <alfredoalea@gmail.com>
2023-03-01 13:45:09 -0500

    Commented out the HTTP request pipeline. Possible solution if this doesn't work, copy the Configure file from the working project

--- DCA_Query_Logger_CloudBased/Program.cs
+++ DCA_Query_Logger_CloudBased/Program.cs
@@ -21,11 +21,11 @@ builder.Services.AddDbContext<DCA_Query_Logger_CloudBasedContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Error");
+//if (!app.Environment.IsDevelopment()) {
+   // app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+  //  app.UseHsts();
+//}
 
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
