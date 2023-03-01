3cf7137b0543bf2a32d7458b03089331c45c03c4
Alfredo Alea <alfredoalea@gmail.com>
2023-03-01 16:17:24 -0500

    returned the Error files to their original place

--- DCA_Query_Logger_CloudBased/Pages/Queries/Error.cshtml.cs
+++ DCA_Query_Logger_CloudBased/Pages/Queries/Error.cshtml.cs
@@ -2,21 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace DCA_Query_Logger_CloudBased.Pages {
+namespace DCA_Query_Logger_CloudBased.Pages.Queries
+{
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel {
+    public class ErrorModel : PageModel
+    {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
 
-        public ErrorModel(ILogger<ErrorModel> logger) {
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
             _logger = logger;
         }
 
-        public void OnGet() {
+        public void OnGet()
+        {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
