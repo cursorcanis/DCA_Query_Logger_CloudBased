bcdc5e73eeb16905304984fecebee83cb39dcbb6
Alfredo Alea <alfredoalea@gmail.com>
2023-03-01 14:50:20 -0500

    Changed the Program.cs file to handle the HTTP request pipeline

--- DCA_Query_Logger_CloudBased/Program.cs
+++ DCA_Query_Logger_CloudBased/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using DCA_Query_Logger_CloudBased.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,11 +22,15 @@ builder.Services.AddDbContext<DCA_Query_Logger_CloudBasedContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment()) {
-   // app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  //  app.UseHsts();
-//}
+  if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error");
+  //  The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+
+
 
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
