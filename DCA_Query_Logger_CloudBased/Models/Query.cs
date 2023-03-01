c2cb049a94595fcddcf982b01a4a2b0bb705dfdf
Alfredo Alea <alfredoalea@gmail.com>
2023-03-01 10:51:22 -0500

    Added the Models folder and properties inside the Query.cs file

---
+++ DCA_Query_Logger_CloudBased/Models/Query.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DCA_Query_Logger_CloudBased.Models {
+    public class Query 
+        {
+        public int Id { get; set; }
+        [Display(Name = "Title of Query")]
+        public string? Title_of_Query { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Type of Query")]
+        public string? Type_of_Query { get; set; }
+        //public decimal Price { get; set; }
+    }
+}
